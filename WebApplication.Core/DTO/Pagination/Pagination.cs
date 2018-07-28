6604ebd42c09086c74e771adf2f4e129529e117d
Mahdi Moeinifar <Mahdi.Moeinifar@Yahoo.com>
2018-07-28 22:52:18 +0430

    Create UsersRepository

---
+++ WebApplication.Core/DTO/Pagination/Pagination.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApplication.Core.DTO.Pagination
+{
+    public class Pagination
+    {
+        public int Page { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+
+        public string Term1 { get; set; }
+
+        public string Term2 { get; set; }
+
+        public string Term3 { get; set; }
+
+        public string Term4 { get; set; }
+
+        public string Term5 { get; set; }
+
+        public string Term6 { get; set; }
+
+        public string Term7 { get; set; }
+
+        public string Term8 { get; set; }
+
+        public string Term9 { get; set; }
+
+        public string Term10 { get; set; }
+
+        public string Term11 { get; set; }
+
+        public string Term12 { get; set; }
+
+        public string Term13 { get; set; }
+
+        public string Term14 { get; set; }
+
+        public string Term15 { get; set; }
+
+        public string Term16 { get; set; }
+
+        public string Term17 { get; set; }
+
+        public string Term18 { get; set; }
+
+        public string Term19 { get; set; }
+
+        public string Term20 { get; set; }
+    }
+}
--- WebApplication.Data/Repository/UsersRepository.cs
+++ WebApplication.Data/Repository/UsersRepository.cs
@@ -1,10 +1,66 @@
-﻿using System;
+﻿using Alamut.Data.Linq;
+using Alamut.Data.Paging;
+using Alamut.Helpers.Linq;
+using Alamut.Data.Structure;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using WebApplication.Core.DTO.Pagination;
+using WebApplication.Core.DTO.Users;
+using AutoMapper.QueryableExtensions;
+using WebApplication.Core.ViewModel.Users;
+using AutoMapper;
+using WebApplication.Core.Entities;
 
 namespace WebApplication.Data.Repository
 {
-    class UsersRepository
+    public class UsersRepository
     {
+        private readonly ApplicationContext _context;
+        public UsersRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public IPaginated<UsersSummaryDTO> GetAll(Pagination vm)
+        {
+            return _context.Users
+                .WhereIf(!string.IsNullOrWhiteSpace(vm.Term1) ||
+                         !string.IsNullOrWhiteSpace(vm.Term2)
+                 , p => (!string.IsNullOrWhiteSpace(vm.Term1) && p.Name != null && p.Name.Contains(vm.Term1)) ||
+                        (!string.IsNullOrWhiteSpace(vm.Term2) && p.Family != null && p.Family.Contains(vm.Term2)))
+                .OrderByDescending(p => p.Id)
+                .ProjectTo<UsersSummaryDTO>()
+                .ToPaginated(new PaginatedCriteria(vm.Page, vm.PageSize));
+        }
+
+        public Users GetById(int id)
+        {
+            return _context.Users.Find(id);
+        }
+
+        public ServiceResult<int> Create(UsersCreateViewModel vm)
+        {
+            var entity = Mapper.Map<Users>(vm);
+            _context.Users.Add(entity);
+            _context.SaveChanges();
+            return ServiceResult<int>.Okay(data: entity.Id, message: "عملیات با موفقیت انجام شد.");
+        }
+
+        public ServiceResult<int> Edit(int id,UsersEditViewModel vm)
+        {
+            var entity = GetById(id);
+            Mapper.Map(vm, entity);
+            _context.SaveChanges();
+            return ServiceResult<int>.Okay(data: id, message: "عملیات با موفقیت انجام شد.");
+        }
+
+        public ServiceResult<int> Delete(int id)
+        {
+            var entity = GetById(id);
+            _context.Users.Remove(entity);
+            return ServiceResult<int>.Okay(data: id, message: "عملیات با موفقیت انجام شد.");
+        }
     }
 }
