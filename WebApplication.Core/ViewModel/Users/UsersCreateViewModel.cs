08baf567586b15dcd71d085a8b5ad5fc604efe5d
Mahdi Moeinifar <Mahdi.Moeinifar@Yahoo.com>
2018-07-28 21:21:49 +0430

    AddProject

---
+++ WebApplication.Core/ViewModel/Users/UsersCreateViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Core.ViewModel.Users
+{
+    /// <summary>
+    /// جدول کاربران
+    /// </summary>
+    public class UsersCreateViewModel
+    {
+        /// <summary>
+        /// کلید اصلی
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// نام
+        /// </summary>
+        [Display(Name = "نام")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// نام خانوادگی
+        /// </summary>
+        [Display(Name = "نام خانوادگی")]
+        [StringLength(50)]
+        public string Family { get; set; }
+
+        /// <summary>
+        /// نام کامل
+        /// </summary>
+        [Display(Name = "نام کامل")]
+        [StringLength(101)]
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// نام کاربری
+        /// </summary>
+        [Display(Name = "نام کاربری")]
+        [StringLength(20)]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// کلمه عبور
+        /// </summary>
+        [Display(Name = "کلمه عبور")]
+        [StringLength(20)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// تصویر کاربر
+        /// </summary>
+        [Display(Name = "تصویر کاربر")]
+        [StringLength(200)]
+        public string Avatar { get; set; }
+
+        /// <summary>
+        /// فعال
+        /// </summary>
+        [Display(Name = "فعال")]
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// تغییر اطلاعات کاربر
+        /// </summary>
+        [Display(Name = "تغییر اطلاعات کاربر")]
+        public bool ChangeProfile { get; set; }
+
+        /// <summary>
+        /// تاریخ ایجاد
+        /// </summary>
+        [Display(Name = "تاریخ ایجاد")]
+        [StringLength(10)]
+        public string CreationDate { get; set; }
+
+        /// <summary>
+        /// تاریخ ویرایش
+        /// </summary>
+        [Display(Name = "تاریخ ویرایش")]
+        [StringLength(10)]
+        public string EditDate { get; set; }
+    }
+}
---
+++ WebApplication.Data/ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication.Core.Entities;
+
+namespace WebApplication.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public virtual DbSet<Users> Users { get; set; }
+
+        //Just for Migration
+        public class TemporaryDbContextFactory : IDesignTimeDbContextFactory<ApplicationContext>
+        {
+            public ApplicationContext CreateDbContext(string[] args)
+            {
+                var builder = new DbContextOptionsBuilder<ApplicationContext>();
+                builder.UseSqlServer("Data Source=.;Initial Catalog=WebApplication;Persist Security Info=True;User ID=sa; Password=95; MultipleActiveResultSets=True");
+                return new ApplicationContext(builder.Options);
+            }
+        }
+    }
+}
