08a40ea373a58405803568aa0cd68edea2fb4adb
Mahdi Moeinifar <Mahdi.Moeinifar@Yahoo.com>
2018-07-28 22:24:01 +0430

    Add-Migration InitialCreate

---
+++ WebApplication.Data/Migrations/20180728175239_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 50, nullable: true),
+                    Family = table.Column<string>(maxLength: 50, nullable: true),
+                    FullName = table.Column<string>(maxLength: 101, nullable: true),
+                    UserName = table.Column<string>(maxLength: 20, nullable: true),
+                    Password = table.Column<string>(maxLength: 20, nullable: true),
+                    Avatar = table.Column<string>(maxLength: 200, nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    ChangeProfile = table.Column<bool>(nullable: false),
+                    CreationDate = table.Column<string>(maxLength: 10, nullable: true),
+                    EditDate = table.Column<string>(maxLength: 10, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
