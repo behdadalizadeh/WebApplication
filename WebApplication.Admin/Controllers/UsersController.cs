1f2a702dda6b2f8f46930f71463a9f4594f5db4a
Mahdi Moeinifar <Mahdi.Moeinifar@Yahoo.com>
2018-07-29 19:05:47 +0430

    Add TempDataExtension to Admin

--- WebApplication.Admin/Controllers/UsersController.cs
+++ WebApplication.Admin/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication.Admin.Helpers;
 using WebApplication.Core.DTO.Pagination;
 using WebApplication.Core.ViewModel.Users;
 using WebApplication.Data.Repository;
---
+++ WebApplication.Admin/Helpers/TempDataExtension.cs
@@ -0,0 +1,14 @@
+﻿using Alamut.Data.Structure;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace WebApplication.Admin.Helpers
+{
+    public static class TempDataExtension
+    {
+        public static void AddResult(this ITempDataDictionary tempData, ServiceResult result)
+        {
+            tempData["ServiceResult.Succeed"] = result.Succeed;
+            tempData["ServiceResult.Message"] = result.Message;
+        }
+    }
+}
